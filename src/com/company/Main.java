bbf2864cfe01ab1699780b78cf084efdcc52bc0c
Chelseaquin <uzonweze@gmail.com>
2020-06-10 22:36:34 +0100

    Assignment marked with some explanations

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,57 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	for(int i = 1; i <= 200; i++){
-	    System.out.println(i);
+        /*Loops are used to create programs that should keep executing till a condition is met.
+         * There are three(3) types of loops in Java
+         * 1. for loops
+         * 2. do while
+         * 3. while
+         *
+         * for loops:
+         * for(initialization; condition; increment/decrement){
+         * statement(s)
+         * }
+         *
+         * when a for loop program is written, the compiler goes into the condition, and keeps executing the program till the condition is met
+         *
+         * */
+        for (int i = 1; i <= 200; i++) {
+            System.out.println(i);
+        }
+
+        int ans = 0;
+        for (int j = 1; j <= 10; j++) {
+            ans += j;
+        }
+        System.out.println(ans);
+
+        /*Infinite Loops
+        * Infinite loops are loops that runs forever, excepte we kill the program, they are mostly
+        * made by writing a condition which can never be met.
+        * */
+        //for example
+        //you can uncomment this to run by pressing ctrl + / after highlighting
+//        for (int i=0; i > -4; i++ ){
+//            System.out.println("Hy");
+//        }
+        /*So the expression above is going to print  "Hy" forever because the condition is going to be greater than minus 4 as long as we keep incrementing
+        * */
+        //another basic example of for loop
+        //you can uncomment this to run by pressing ctrl + / after highlighting
+//        for (; ; ) {
+//            System.out.println("Hello world");
+//        }
+        /*
+        * Enhanced For loop
+        * Enhanced for loop is useful when you want to iterate Array/Collections, it is easy to write and understand.
+        * Let’s take the same example that we have written above and rewrite it using enhanced for loop.*/
+        //comment the above codes to run this
+        String scastudents[]={"Tilda","Nikki", "ChelseaQuin", "Love", "Mary"};
+        for (String students : scastudents) {
+            System.out.println(students);
+        }
     }
-    int ans = 0;
-	for(int j = 1; j <= 10; j++){
-	    ans += j;
-    }System.out.println(ans);
-    }
+
 }
 /*
 1. What is instance variable: instance variable are variables declared inside a class
@@ -32,3 +75,4 @@ vi.  long data type
 vii. float data type
 viii.double data type
  */
+//Score: 10
